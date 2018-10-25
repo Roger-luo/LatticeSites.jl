9e699f668ac3de89fcd398d6eb565edec2f1f6b4
Roger-luo <hiroger@qq.com>
2018-10-24 23:39:40 -0400

    refactor the whole thing!
    - use immutable for sinlge element instead of a custom array
    - define a new HilbertSpace type for iteration through the space

---
+++ src/spins.jl
@@ -0,0 +1,58 @@
+export up, down, Bit, Spin, Half
+
+abstract type AbstractSite{T} end
+
+Base.show(io::IO, s::AbstractSite) = show(io, value(s))
+
+Base.:(==)(lhs::AbstractSite, rhs::Number) = value(lhs) == rhs
+Base.:(==)(lhs::Number, rhs::AbstractSite) = lhs == value(rhs)
+
+abstract type BinarySite{T} <: AbstractSite{T} end
+
+"""
+    up(site) -> site
+    up(site_type) -> site
+up tag for this label. e.g. `1` for `Bit`, `0.5` for `Half`.
+"""
+function up end
+
+"""
+    down(site) -> site
+    down(site_type) -> site
+down tag for this label. e.g. `0` for `Bit`, `-0.5` for `Half`.
+"""
+function down end
+
+value(s::BinarySite) = s.value
+
+struct Bit{T} <: BinarySite{T}
+    value::T
+end
+
+struct Spin{T} <: BinarySite{T}
+    value::T
+end
+
+struct Half{T} <: BinarySite{T}
+    value::T
+end
+
+up(::Type{Bit{T}}) where T = Bit(one(T))
+up(::Type{Spin{T}}) where T = Spin(one(T))
+up(::Type{Half{T}}) where T = Half(T(0.5))
+
+down(::Type{Bit{T}}) where T = Bit(zero(T))
+down(::Type{Spin{T}}) where T = Spin(-one(T))
+down(::Type{Half{T}}) where T = Half(-T(0.5))
+
+# Base.rand(::Type{Bit{T}}) where T = Bit{T}(rand(Bool))
+# Base.rand(::Type{Spin{T}}) where T = Spin{T}(2 * rand(Bool) - 1)
+# Base.rand(::Type{Half{T}}) where T = Half{T}(rand(Bool) - 0.5)
+
+Random.rand(rng::Random.AbstractRNG, sp::Random.SamplerType{Bit{T}}) where T = Bit{T}(rand(rng, Bool))
+Random.rand(rng::Random.AbstractRNG, sp::Random.SamplerType{Spin{T}}) where T = Spin{T}(2 * rand(rng, Bool) - 1)
+Random.rand(rng::Random.AbstractRNG, sp::Random.SamplerType{Half{T}}) where T = Half{T}(rand(rng, Bool) - 0.5)
+
+include("roundings.jl")
+include("conversions.jl")
+include("arraymath.jl")
