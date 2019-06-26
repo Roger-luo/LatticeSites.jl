aa92efdbfd5e1b1892778a4b0fe6e7a8d9e12cf4
Roger-luo <hiroger@qq.com>
2019-06-26 14:10:49 -0400

    update

--- src/lattices.jl
+++ src/lattices.jl
@@ -1,7 +1,7 @@
 using Lattices
 
 ups(::Type{ST}, ltc::Lattices.AbstractLattice) where ST = ups(ST, size(ltc))
-downs(::Type{ST}, ltc::Lattices.AbstractLattice) where ST = ups(ST, size(ltc))
+downs(::Type{ST}, ltc::Lattices.AbstractLattice) where ST = downs(ST, size(ltc))
 Random.rand(::Type{ST}, ltc::Lattices.AbstractLattice) where ST = rand(ST, size(ltc))
 
 ups(ltc::Lattices.AbstractLattice) = ups(Bit{Int}, ltc)
